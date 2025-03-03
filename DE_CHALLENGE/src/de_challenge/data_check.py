11f5fbb1ff8c309352dc7478c84f1eba38517eda
khandelwalkomal <khandelwalkomal786@gmail.com>
2025-03-03 23:16:11 +0530

    adding database and lock file

--- DE_CHALLENGE/src/de_challenge/data_check.py
+++ DE_CHALLENGE/src/de_challenge/data_check.py
@@ -1,17 +1,22 @@
 import sqlite3
 
 
-def verify_data():
+def verify_data(tbl_nm):
     conn = sqlite3.connect('product_ratings.db')
     c = conn.cursor()
-    result = c.execute('''
-        select * from ratingMonthlyAggregates limit 10;
+    result = c.execute(f'''
+        select * from {tbl_nm} limit 10;
     ''').fetchall()
     conn.close()
     return result
 
 
 if __name__ == '__main__':
-    count = verify_data()
-    print(count)
+    rating_data = verify_data("ratings")
+    print(f"ratings data: {rating_data}")
+
+    aggregates_data = verify_data("ratingMonthlyAggregates")
+    print(f"aggregates data: {aggregates_data}")
+
+
 
